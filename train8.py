378a5477753f0cbe03f8f9afaef889a919d39ca1
Computer Guy <computer.guybuilder@gmail.com>
2021-03-13 16:05:54 +0530

    Update and rename train7.py to train8.py

--- train8.py
+++ train8.py
@@ -24,11 +24,6 @@ from argparse import ArgumentParser
 
 
 class BaseTransformersCLICommand(ABC):
-    @staticmethod
-    @abstractmethod
-    def register_subcommand(parser: ArgumentParser):
-        raise NotImplementedError()
-
     @abstractmethod
     def run(self):
         raise NotImplementedError()
